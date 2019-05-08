e10bebedbc8a2f481b963fee6dcef9040616acff
ravi <ravishankar.sla@gmail.com>
2019-05-08 08:39:18 +0530

    All Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,65 @@
+import 'package:ecommerce/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-import './pages/home_page.dart';
 
-void main() => runApp(new MaterialApp(
-  home: new HomePage(),
-));
+void main() => runApp(new MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+          primaryColor: Colors.white,
+          primaryColorDark: Colors.white30,
+          accentColor: Colors.blue
+
+      ),
+      home: new MyHomePage(title: 'Groceries'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Home_screen()));
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(color: Colors.white),
+      child: new Container(
+        color: Colors.black12,
+        margin: new EdgeInsets.all(30.0),
+        width: 250.0,
+        height: 250.0,
+        child: new Image.asset(
+          'images/logo.png',
+        ),
+      ),
+    );
+  }
+
+}
--- lib/pages/page.dart
+++ lib/pages/page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Page extends StatelessWidget {
 
   final String title;
